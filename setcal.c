a3638a0387067239204fe673eaeb491db9e0328f
Cheloved <27046990+Cheloved@users.noreply.github.com>
2021-11-04 15:32:40 +0300

    7

--- setcal.c
+++ setcal.c
@@ -9,11 +9,13 @@ enum DataType{ U, S, R, C };
 typedef char* Element;
 
 
-typedef struct RelationEl_s RelationElements;
+typedef struct RelationEl_s RelationElement;
 struct RelationEl_s
 {
     Element leftElement;
-    Element RightElement;
+    Element rightElement;
+    int leftIndex;
+    int rightIndex;
 };
 
 typedef struct Line_s Line;
@@ -22,7 +24,8 @@ struct Line_s
     enum DataType            _type;
     int                         id;
     Element*              elements; 
-    RelationElements*    relations;
+    int*                   indexes;
+    RelationElement**    relations;
     int               elementCount;
 }; 
 
@@ -44,9 +47,12 @@ int readLine(char** line, FILE* file, long fileSize)
 
     while ( (c = getc(file)) != '\n' )
     {
-        if ( ftell(file) + 1 >= fileSize )
-            lastLine = 1;
         len++;
+        if ( ftell(file) + 1 >= fileSize )
+        {
+            lastLine = 1;
+            break;
+        }
     }
     if ( len == 0 )
         return 1;
@@ -148,45 +154,52 @@ int splitElements(Line* line, char* str)
 
 int subdivideRelations(Line* line)
 {
-    line->relations = (RelationElements*)calloc(line->elementCount, sizeof(RelationElements*));
+    line->relations = (RelationElement**)calloc(line->elementCount, sizeof(RelationElement**));
     for ( int i = 0; i < line->elementCount; i++ )
     {
         int spaceIndex = findSpaceAfter(0, line->elements[i]);
         int len = strLen( line->elements[i] );
-        line->relations[i].leftElement = calloc(spaceIndex + 1, sizeof(char));
-        line->relations[i].RightElement = calloc(len - spaceIndex, sizeof(char));
+        line->relations[i] = (RelationElement*)calloc(1, sizeof(RelationElement*));
+        line->relations[i]->leftElement = calloc(spaceIndex + 1, sizeof(char));
+        line->relations[i]->rightElement = calloc(len - spaceIndex, sizeof(char));
 
         for ( int j = 0; j < spaceIndex; j++ )
-            line->relations[i].leftElement[j] = line->elements[i][j];
+            line->relations[i]->leftElement[j] = line->elements[i][j];
+        line->relations[i]->leftElement[spaceIndex] = '\0';
 
         for ( int j = spaceIndex + 1; j < len; j++ )
-            line->relations[i].RightElement[ j - spaceIndex - 1] = line->elements[i][j];
+            line->relations[i]->rightElement[ j - spaceIndex - 1] = line->elements[i][j];
     }
     return 0;
 }
 
 int getData(Line* lineBuffer, FILE* file, long fileSize, int id)
 {
-    // Read a line of data
+    // Read a line of data and handle a return value
     char* line;
-    int e = readLine(&line, file, fileSize);  // Return value placeholder
+    int e = readLine(&line, file, fileSize);
     if ( e == -1 )  // Handle error
         return -1;
-    printf("%s\n", line);
+
+    // Print line
+    printf("\n%s\n", line);
 
     /* Get data type */
     enum DataType _type = U;
     switch (line[0]) 
     {
-        case 'U': { _type = U; break; }
-        case 'S': { _type = S; break; }
-        case 'R': { _type = R; break; }
-        case 'C': { _type = C; break; }
+        case 'U': { _type = U; break; } // Universe
+        case 'S': { _type = S; break; } // Set
+        case 'R': { _type = R; break; } // Relation
+        case 'C': { _type = C; break; } // Calculation
         default: { fprintf(stderr, "Error: unknown identifier\n"); return -1; }
     }
 
+    // Set struct members
     lineBuffer->_type = _type;
-    lineBuffer->id    = id;
+    lineBuffer->id    = id + 1;
+
+    // 
     lineBuffer->elementCount = getElementCount(line, lineBuffer->_type);
     if ( splitElements(lineBuffer, line) != 0 )
        return -1;
@@ -208,8 +221,108 @@ long getFileSize(FILE* file)
     return fileSize;
 }
 
+int isEqual( Element e1, Element e2 )
+{
+    int len1 = strLen( (char*)e1);
+    int len2 = strLen( (char*)e2);
+
+    if ( len1 != len2 )
+        return 0;
+    
+    for ( int i = 0; i < len1; i++ )
+        if ( e1[i] != e2[i] )
+            return 0;
+    
+    return 1;
+}
+
+int indexOf(Element e, Line* universe)
+{
+    for ( int i = 0; i < universe->elementCount; i++ )
+    {
+        if ( isEqual(e, universe->elements[i]) )
+            return i;
+    }
+    printf("Error: there's not %s in Universe", e);
+    return -1;
+}
+
+int assingnUniverse(Line* line)
+{
+    line->indexes = calloc(line->elementCount, sizeof(int));
+    for ( int i = 0; i < line->elementCount; i++ )
+        line->indexes[i] = i;
+    return 0;
+}
+
+int assignElementIndexes(Line* line, Line* universe)
+{
+    if ( line->_type == C )
+        return 0;
+
+    if ( line->_type == R )
+    {
+        for ( int i = 0; i < line->elementCount; i++ )
+        {
+            int idxLeft = indexOf(line->relations[i]->leftElement, universe);
+            int idxRight = indexOf(line->relations[i]->rightElement, universe);
+            if ( (idxLeft == -1) || (idxRight == -1) )
+                return -1;
+
+            line->relations[i]->leftIndex = idxLeft;
+            line->relations[i]->rightIndex = idxRight;
+        }
+    } else {
+        line->indexes = calloc(line->elementCount, sizeof(int));
+        for ( int i = 0; i < line->elementCount; i++ )
+        {
+            int idx = indexOf(line->elements[i], universe);
+            if ( idx == -1 )
+                return -1;
+            else
+                line->indexes[i] = idx;
+        }
+    }
+    return 0;
+}
+
+void printDataLine(Line line)
+{
+    printf("Data line %d\n", line.id);
+    switch (line._type) 
+    {
+        case U: printf("  Type: Universe\n"); break;
+        case S: printf("  Type: Set\n"); break;
+        case R: printf("  Type: Relation\n"); break;
+        case C: printf("  Type: Calculation\n"); break;
+    }
+    printf("  Element count: %d\n", line.elementCount);
+    switch (line._type) 
+    {
+        case R: {
+            for ( int i = 0; i < line.elementCount; i++ )
+            {
+                printf("    Element %d: (%s, %s)", i, line.relations[i]->leftElement, line.relations[i]->rightElement);
+                printf("; Indexes: (%d, %d)\n", line.relations[i]->leftIndex, line.relations[i]->rightIndex);
+            }
+            break;
+        }    
+        case S: {
+            for ( int i = 0; i < line.elementCount; i++ )
+                printf("    Element %d: %s; Index: %d\n", i, line.elements[i], line.indexes[i]);
+            break;
+        }
+        default: {
+            for ( int i = 0; i < line.elementCount; i++ )
+                printf("    Element %d: %s\n", i, line.elements[i]);
+            break;
+        }
+    }
+}
+
 int readFile(char* path)
 {
+    // Open file and check if it exists
     FILE* file;
     if ( (file = fopen(path, "rt")) == NULL )
     {
@@ -220,27 +333,48 @@ int readFile(char* path)
     //  Get number of characters in file
     long fileSize = getFileSize(file);
 
+    // According to terms
+    // Max number of lines is 1000
     Line lines[1000];
+
+    // Count of readed lines
     int  totalLines = 0;
 
+    // Buffer for current line
     Line lineBuffer;
 
-    int e = 0; // Return value placeholder
+    char* setFunctionsUnarNames[2] = {"empty", "card"};
+    char* setFunctionsBinarNames[7] = {"complement", "union", "intersect", "minus", "subseteq", "subset", "equals"};
+    void(*setFunctionsUnar)(Line line); // = {&empty, &card};
+    void(*setFunctionsBinar)(Line lineA, Line lineB); // = {&complement, &union, &intersect, &minus, &subseteq, &subset, &equals}
+
+    char* relFunctionNames[10] = { "reflexive", "symmetric", "antisymmetric", "transitive", "function", "domain", "codomain", "injective", "surjective", "bijective" };
+
+    // Return value placeholder
+    int e = 0;     
     while ( 1 )
     {
+        // Read data and handle an error
         e = getData(&lineBuffer, file, fileSize, totalLines);
         if ( e == -1 )
             return -1;
         
+        // Set line
         lines[totalLines] = lineBuffer;
-        printf("  Id: %d\n  Type: %d\n  Element count: %d\n", lines[totalLines].id, lines[totalLines]._type, lines[totalLines].elementCount);        
-        printf("  Elements:\n");
 
-        for ( int i = 0; i < lines[totalLines].elementCount; i++ )
-            printf("    Element %d: %s\n", i, lines[totalLines].elements[i]);
+        // Assign numbers to elements, since working with numbers
+        // is faster than working with strings
+        if ( totalLines == 0 )
+            assingnUniverse( &lines[totalLines] );
+        else
+            assignElementIndexes(&lines[totalLines], &lines[0]);
 
+        printDataLine(lines[totalLines]);
+
+        // Increase number of readed lines
         totalLines++;
 
+        // If file has been readed till the end
         if ( e == 1 )
             break;
     }
@@ -250,6 +384,7 @@ int readFile(char* path)
 
 int readArgs(int argc, char* argv[], char** path)
 {
+    // Handle wrong arguments
     if ( argc < 2 )
     {
         fprintf(stderr, "Error: not enough arguments\n");
@@ -261,16 +396,21 @@ int readArgs(int argc, char* argv[], char** path)
         return -1;
     }
 
+    // Copy file path from argument to variable
     strcpy( *path, argv[1] );
     return 0;
 }
 
 int main(int argc, char* argv[])
 {
+    // Declaration of file path
     char* path;    
+
+    // Read arguments from command line
     if ( readArgs(argc, argv, &path) != 0 )
         return -1;
 
+    // Read file
     if ( readFile(path) != 0 )
         return -1;
         
